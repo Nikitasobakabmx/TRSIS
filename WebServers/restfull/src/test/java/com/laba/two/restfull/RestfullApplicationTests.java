72d12504dbd56a06275c9973179499b6db47d858
Nikittossii <NikitaSoabakabmx@gmail.com>
2019-11-12 04:12:46 +0300

    RESTFULL

---
+++ WebServers/restfull/src/test/java/com/laba/two/restfull/RestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laba.two.restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
