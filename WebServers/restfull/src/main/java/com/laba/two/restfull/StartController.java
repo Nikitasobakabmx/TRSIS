f6e9d1f1686bb151a65a1ac2e13a43bb0e278b0e
Nikittossii <NikitaSoabakabmx@gmail.com>
2019-11-19 13:03:45 +0300

    AJAX

--- WebServers/restfull/src/main/java/com/laba/two/restfull/StartController.java
+++ WebServers/restfull/src/main/java/com/laba/two/restfull/StartController.java
@@ -10,9 +10,8 @@ import java.util.Optional;
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/games")
 class StartController {
-
     @Autowired
     private GameService gameService;
 
@@ -25,21 +24,25 @@ class StartController {
 
     @RequestMapping(method=RequestMethod.DELETE)
     public ResponseEntity<Object> delete(@RequestParam(value = "id") String id){
+        System.out.println("Delete " + id);
         Optional<GamesEntity> tmp = gameService.findById(id);
         gameService.deleteById(id);
         return ResponseEntity.ok(tmp);
     }
+
     @RequestMapping(method=RequestMethod.POST, consumes = "application/json", produces = { "application/json", "application/xml" })
     public @ResponseBody ResponseEntity<Object> append(@RequestBody GamesEntity entity){
+        System.out.println(entity.getName());
         gameService.append(entity);
         return ResponseEntity.ok(entity);
     }
 
     @RequestMapping(method = RequestMethod.PUT, consumes = "application/json")
     public @ResponseBody ResponseEntity<Object> update(@RequestBody GamesEntity entity){
+        System.out.println(entity.getCost());
+        System.out.println(entity.getId());
         GamesEntity tmp = gameService.update(entity);
         return ResponseEntity.ok(tmp);
     }
-
 }
 
--- WebServers/restfull/src/main/java/com/laba/two/restfull/config/SecurityConfiguration.java
+++ WebServers/restfull/src/main/java/com/laba/two/restfull/config/SecurityConfiguration.java
@@ -17,7 +17,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/home/**", "/hello/**").permitAll()
+                .antMatchers("/**", "/games/**", "/hello/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
